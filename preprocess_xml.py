e2f75769a00df4210ab9fa1d94493c4a4a819419
iansteyn <iansteyn03@gmail.com>
2026-03-19 14:51:41 -0700

    Add several modifications to content:encoded tags - replace headings, replace empty paragraphs, remove style tags, add TODOs

--- preprocess_xml.py
+++ preprocess_xml.py
@@ -4,11 +4,11 @@ TODO: uninstall lxml and dependencies
 """
 # -----------------------------------------------
 # IMPORTS AND WARNING FILTERS
-from bs4 import BeautifulSoup, XMLParsedAsHTMLWarning
+from bs4 import BeautifulSoup #, XMLParsedAsHTMLWarning
 from bs4.element import CData
-import warnings
+# import warnings
 
-warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
+# warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
 # -----------------------------------------------
 # FUNCTIONS
@@ -18,18 +18,49 @@ def create_better_content_tag(content_tag):
 
         inner_soup = BeautifulSoup(content_tag.string, 'html.parser')
 
-        # modify
-        new_tag = inner_soup.new_tag("p")
-        new_tag.string = "this has been modified"
-        inner_soup.insert(0, new_tag)
-
-        # Delete that summary block section
+        # MOD: Delete that summary block section # ORDER MATTERS
         blocks_to_remove = inner_soup.find_all("div", class_="summary-block-wrapper")
         
         for block in blocks_to_remove:
             block.extract()
 
-        return CData(str(inner_soup))
+        # MOD: replace empty <p> tags with <br/> # ORDER MATTERS
+            # TODO: may have to check whether more <br>s are needed, eg after lists and titles
+        p_tags = inner_soup.find_all('p')
+
+        for p in p_tags:
+            if p.get_text(strip=True) == '':
+                br = inner_soup.new_tag('br')
+                p.replace_with(br)
+
+        # MOD: Replace <p class="sqsrte-large"> with <h4> # ORDER MATTERS
+        large_text_tags = inner_soup.find_all("p", class_="sqsrte-large")
+
+        for tag in large_text_tags:
+            tag.name = 'h4'
+            del tag['class']
+
+        # TODO: replace <hr> with <p>---</p>
+        # TODO: remove uneccesary divs
+            # learning this will help me with the below task
+
+        # TODO: simplify list items
+            # For some reason, each list item is its own list, AND is wrapped inside of a <p> on the inside. 
+            # I want to simplify this. However, some lists are also structured more normally
+        
+        # TODO: append links extracted from excerpt
+        
+        # TODO: remove all remaining extra classes and styles
+
+        # MOD: delete all style and other extraneous tags (can probably happen near the end)
+        for tag in inner_soup.find_all(True):
+            if tag.has_attr('style'):
+                del tag['style']
+            if tag.has_attr('data-rte-preserve-empty'):
+                del tag['data-rte-preserve-empty']
+
+
+        return CData(str(inner_soup.prettify()))
 
 def create_better_excerpt_tag(excerpt_tag):
     '''
@@ -63,11 +94,10 @@ for item in items:
     excerpt_tag = item.find('excerpt:encoded')
     excerpt_tag.string = create_better_excerpt_tag(excerpt_tag)
 
-# soup.prettify()
 # --------------------------------
 # FINISH
 
 print(f"Modified {len(items)} test posts.")
 
 with open(OUTPUT_FILE_PATH, 'w', encoding='utf-8') as file:
-    file.write(str(soup))
+    file.write(str(soup.prettify()))
