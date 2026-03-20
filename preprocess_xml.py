1a6a22fb7f642a6b8692a4fecb4f8de7253a7942
iansteyn <iansteyn03@gmail.com>
2026-03-20 15:53:16 -0700

    remove XMLParseAsHTMLWarning import and warning suppresion
    no longer necessary since we are back to using the xml parser

--- preprocess_xml.py
+++ preprocess_xml.py
@@ -6,11 +6,8 @@ General Notes
 """
 # -----------------------------------------------
 # IMPORTS AND WARNING FILTERS
-from bs4 import BeautifulSoup, Tag#, XMLParsedAsHTMLWarning
+from bs4 import BeautifulSoup, Tag
 from bs4.element import CData
-# import warnings
-
-# warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
 # ---------------------------------------------------
 # CONSTANTS/CONFIG
